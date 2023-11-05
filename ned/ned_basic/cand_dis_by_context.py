2988880b2992d75fa28e1f12113b921fd3d131ea
Krzysztof Nazar <s184698@student.pg.edu.pl>
2023-11-05 10:43:09 +0100

    add normalization of TF-IDF vectors (magnitude)

--- ned/ned_basic/cand_dis_by_context.py
+++ ned/ned_basic/cand_dis_by_context.py
@@ -1,8 +1,13 @@
 """
     Disambiguate candidates for each entity based on context similarity.
 """
+
+# TODO: add normalization of the final scores (from 0 - min score, to 1 - max score)
+
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
+from sklearn.preprocessing import normalize
 
 
 def disambiguate_by_context_sentence_and_abstract(entities):
@@ -16,14 +21,6 @@ def disambiguate_by_context_sentence_and_abstract(entities):
     :param entities: List of entities found in the text, each with associated candidates.
     :return: List of entities with candidates ranked from the most probable to least probable, based on context.
     """
-    # TODO: add normalization of magnitude:
-    #   Normalization is an optional step in the TF-IDF calculation,
-    #   and it's often used to ensure that the vectors
-    #   have a unit length (a magnitude of 1).
-    #   Normalization can be achieved using various techniques,
-    #   with the most common one being L2 normalization (also known as Euclidean normalization).
-
-    # TODO: add normalization of the final scores (from 0 - min score, to 1 - max score)
 
     # Create a dictionary to store the first sentence of each abstract by URI
     abstracts = {}
@@ -42,16 +39,19 @@ def disambiguate_by_context_sentence_and_abstract(entities):
     tfidf_vectorizer = TfidfVectorizer()
     tfidf_matrix = tfidf_vectorizer.fit_transform(all_sentences)
 
+    tfidf_matrix_normalized = normalize_tfidf_vectors_magnitude(tfidf_matrix)
+
     # Iterate through entities and calculate similarity
-    for entity in entities:
+    for i, entity in enumerate(entities):
         if entity.candidates:
             entity_sentence = entity.sentence_text  # Sentence of the entity
             if entity_sentence:
-                tfidf_entity = tfidf_vectorizer.transform([entity_sentence])
+                tfidf_entity = tfidf_matrix_normalized[i]  # Retrieve the normalized vector for the entity
 
                 for candidate in entity.candidates:
                     candidate_sentence = abstracts.get(candidate.uri, '')  # Get the abstract of the candidate
-                    tfidf_candidate = tfidf_vectorizer.transform([candidate_sentence])
+                    tfidf_candidate = tfidf_matrix_normalized[all_sentences.index(candidate_sentence)]  # Retrieve the normalized vector for the candidate
+
                     similarity_score = linear_kernel(tfidf_entity, tfidf_candidate).flatten()[0]
 
                     # Store the score in the candidate instance
@@ -59,3 +59,21 @@ def disambiguate_by_context_sentence_and_abstract(entities):
                     candidate.cand_dis_current_score += similarity_score
 
     return entities
+
+
+def normalize_tfidf_vectors_magnitude(tfidf_matrix):
+    """
+    Normalize TF-IDF vectors using L2 normalization (Euclidean normalization).
+
+    Normalization ensures that the TF-IDF vectors
+    have a consistent unit length (a magnitude of 1) regardless of the text length.
+
+    By normalizing TF-IDF vectors, we ensure that the vectors are directly comparable
+    and that the results of similarity measurements are more robust and reliable.
+
+    :param tfidf_matrix: The TF-IDF matrix containing TF-IDF vectors for documents.
+    :return: Normalized TF-IDF matrix with unit-length vectors.
+    """
+    # L2 normalization (Euclidean normalization) scales the vectors to have a unit length (magnitude of 1)
+    tfidf_matrix_normalized = normalize(tfidf_matrix, norm='l2', axis=1)
+    return tfidf_matrix_normalized
