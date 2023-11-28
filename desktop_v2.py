b4551f24e890297bf644bdfabb16b49927ed67f5
turtlewneck <michal.wnek01@gmail.com>
2023-11-28 02:28:51 +0100

    fix desktop app
    Fixed some issues with the desktop app. Added text input. added ner and ned selection. Added ner and ned functionality whether to use them or not.

--- desktop_v2.py
+++ desktop_v2.py
@@ -1,6 +1,6 @@
 import sys
 # from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QHBoxLayout, QLabel, QPushButton
-from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
@@ -28,12 +28,6 @@ class DesktopApplication(QMainWindow):
         self.selectedNED = 0
         '''Selected NED Algorithm, default is 0'''
 
-        self.nerAlgorithms = ['one', 'two', 'threeeeee']
-        '''NER Algorithms'''
-
-        self.nedAlgorithms = ['four', 'five', 'six']
-        '''NED Algorithms'''
-
         tfile = open(f"./file/template.xhtml", mode= "r" , encoding="utf-8")
         template = tfile.read()
         tfile.close()
@@ -51,6 +45,18 @@ class DesktopApplication(QMainWindow):
 
         self.nedAlgorithms = self.EL.get_neds()
         '''NED Algorithms'''
+
+        self.is_ner_used = False
+        '''Check if ner is used'''
+
+        self.is_ned_used = False
+        '''Check if ned is used'''
+
+        self.text_from_file = None
+        '''Check if the txt for nerd is inside a variable or inside a file'''
+
+        self.textbox_text = None
+        '''Text from the texbox component'''
 
 
         self.setStyleSheet('''
@@ -73,153 +79,189 @@ class DesktopApplication(QMainWindow):
                 background-color: #424242;
                 color: #ffffff;
             }
+            QCheckBox {
+                color: #ffffff;
+            }
         ''')
 
-
-
-        
         
 
-        self.setWindowTitle("HTML Viewer")
-        # self.setWindowFlag(Qt.FramelessWindowHint)
+        self.setWindowTitle("NERD")
         self.setGeometry(constant.APP_POS_X, constant.APP_POS_Y, constant.APP_WIDTH, constant.APP_HEIGHT)  # Set initial position (x, y) and size (width, height)
 
         # Create the main widget and set it as the central widget
         central_widget = QWidget(self)
         self.setCentralWidget(central_widget)
+
         # Create horizontal layout
         layout = QHBoxLayout(central_widget)
-        self.setLayout(layout)
         
 
         #--------------MANAGING LEFT PANEL--------------#
         label1 = QLabel("", self)
-        label1.setFixedSize(380, self.height())
+        label1.setFixedSize(300, self.height())
+
+        # EKSERYMENT
+        left_layout_widget = QWidget(self)
+        left_layout = QVBoxLayout(left_layout_widget)
+        left_layout.addWidget(label1)
+        left_layout.setContentsMargins(0,0,0,0)
+        #left_layout.addWidget(label1)
+        left_layout_widget.setFixedSize(300, self.height())
+
 
         # TEXTBOX
         self.textbox = QPlainTextEdit(self)
-        self.textbox.move(100, 20)
+        self.textbox.move(50, 20)
         self.textbox.resize(constant.ELM_WIDTH, constant.ELM_HEIGHT * 4)
 
         # LOAD TEXBOX TEXT BUTTON
         self.button_texbox = QPushButton('Load text', self)
-        self.button_texbox.move(100,140)
+        self.button_texbox.move(50,140)
         self.button_texbox.resize(constant.ELM_WIDTH, constant.ELM_HEIGHT)
+        self.button_texbox.clicked.connect(self.on_load_textbox)
 
         # Load button
         self.button_load = QPushButton('Load file:', self)
         self.button_load.setToolTip('laod file to perform nerd on')
-        self.button_load.move(100,180)
+        self.button_load.move(50,180)
         self.button_load.resize(constant.ELM_WIDTH, constant.ELM_HEIGHT)
-        self.button_load.clicked.connect(self.on_load)
+        self.button_load.clicked.connect(self.on_load_file)
         '''Load text button'''
 
-        # NER button
-        self.button_select_ner = QPushButton('Select NER:', self)
-        self.button_select_ner.setToolTip('Select NER algorithm')
-        self.button_select_ner.move(100,240)
-        self.button_select_ner.resize(constant.ELM_WIDTH, constant.ELM_HEIGHT)
-        self.button_select_ner.clicked.connect(self.on_select_ner)
+        self.button_select_ner = QComboBox(self)
+        self.button_select_ner.addItems(self.nerAlgorithms)
+        self.button_select_ner.move(50,240)
+        self.button_select_ner.resize(120, constant.ELM_HEIGHT)
+        self.button_select_ner.currentIndexChanged.connect(self.on_select_ner)
 
-        self.ner_menu = QMenu(self)
-        i = 0
-        for ner_alg in self.nerAlgorithms:
-            option = QAction(ner_alg, self)
-            option.triggered.connect(lambda: self.on_select_ner(i))
-            self.ner_menu.addAction(option)
-            i = i + 1
-
-        self.button_select_ner.setMenu(self.ner_menu)
+        self.checkbox_ner = QCheckBox('Use NER', self)
+        self.checkbox_ner.move(180,240)
+        self.checkbox_ner.resize(60, constant.ELM_HEIGHT)
+        self.checkbox_ner.stateChanged.connect(self.set_ner)
 
 
-        self.button_select_ned = QPushButton('Select NED:', self)
-        self.button_select_ned.setToolTip('Select NED algorithm')
-        self.button_select_ned.move(100,280)
-        self.button_select_ned.resize(constant.ELM_WIDTH, constant.ELM_HEIGHT)
-        self.button_select_ned.clicked.connect(self.on_select_ned)
+        self.button_select_ned = QComboBox(self)
+        self.button_select_ned.addItems(self.nedAlgorithms)
+        self.button_select_ned.move(50,280)
+        self.button_select_ned.resize(120, constant.ELM_HEIGHT)
+        self.button_select_ned.currentIndexChanged.connect(self.on_select_ned)
 
-        self.ned_menu = QMenu(self)
-        j = 0
-        for ned_alg in self.nedAlgorithms:
-            option = QAction(ned_alg, self)
-            # option.
-            option.triggered.connect(lambda: self.on_select_ned(ned_alg))
-            self.ned_menu.addAction(option)
-            j = j + 1
-
-        self.button_select_ned.setMenu(self.ned_menu)
+        self.checkbox_ned = QCheckBox('Use NED', self)
+        self.checkbox_ned.move(180,280)
+        self.checkbox_ned.resize(60, constant.ELM_HEIGHT)
+        self.checkbox_ned.stateChanged.connect(self.set_ned)
         '''Choice component (ner and ned choice)'''
 
         self.button_start = QPushButton('Start', self)
         self.button_start.setToolTip('start algorithms')
-        self.button_start.move(100,320)
+        self.button_start.move(50,320)
         self.button_start.resize(constant.ELM_WIDTH, constant.ELM_HEIGHT)
         self.button_start.clicked.connect(self.on_start)
         '''Start button'''
 
-        self.button_save = QPushButton('Save HTML', self)
-        self.button_save.setToolTip('Save HTML file')
-        self.button_save.move(100,640)
-        self.button_save.resize(constant.ELM_WIDTH, constant.ELM_HEIGHT)
-        self.button_save.clicked.connect(self.on_save)
-        '''Save button'''
+        self.button_dataset = QPushButton('Load Dataset', self)
+        self.button_dataset.setToolTip('')
+        self.button_dataset.move(50,640)
+        self.button_dataset.resize(constant.ELM_WIDTH, constant.ELM_HEIGHT)
+        self.button_dataset.clicked.connect(self.on_load_dataset)
+        '''Load Dataset button'''
+
+        self.button_ned_test = QPushButton('Start NED Test', self)
+        self.button_ned_test.setToolTip('')
+        self.button_ned_test.move(50,680)
+        self.button_ned_test.resize(constant.ELM_WIDTH, constant.ELM_HEIGHT)
+        self.button_ned_test.clicked.connect(self.on_start_ned_test)
+        '''Start Ned Test button'''
 
         #--------------MANAGING MIDDLE PANEL--------------#
-        
-
 
         # HTML section
-        self.app_html_section = HTMLWindow(int(self.width()/2), self.height())
+        # self.app_html_section = HTMLWindow(int(self.width()/2), self.height())
+        # self.app_html_section.setContentsMargins(0,0,0,0)
+        
+        #self.app_html_section = QWebEnginePage(self)
+        self.app_html_section = QWebEngineView(self)
+        self.app_html_section.setFixedSize(470, self.height())
+        self.app_html_section.setHtml(template)
+        
+        # self.setCentralWidget(self.app_html_section)
+        # self.setFixedSize(width, height)
+    
+    
 
 
+        #--------------MANAGING RIGHT PANEL--------------#
         # DBPedia section
-        label3 = QLabel("", self)
-        label3.setFixedSize(380, self.height())
+        self.dbpedia = QWebEngineView(self)
+        self.dbpedia.setFixedSize(470, self.height())
+        self.app_html_section.setPage(CustomWebEnginePage(self.dbpedia, self))
+        # self.dbpedia.setHtml(template)
+        # self.setCentralWidget(self.dbpedia)
+        # self.setFixedSize(380, self.height())
+        # self.dbpedia.page().
+        #label3 = QLabel("", self)
+        #label3.setFixedSize(380, self.height())
         
 
-
-        # Add sections to layout
-        layout.addWidget(label1)
+        
+        
+        layout.addWidget(left_layout_widget)
         layout.addWidget(self.app_html_section)
-        layout.addWidget(label3)
-        
-
-    def show_menu(self):
-        # Show the menu at the button's position
-        self.menu.exec_(self.button.mapToGlobal(self.button.rect().bottomLeft()))
-
-    def menu_action(self, option):
-        print(f'Chose: {option}')
+        #layout.addWidget(label3)
+        layout.addWidget(self.dbpedia)
+    
+        self.setLayout(layout)
+      
 
 
     @pyqtSlot()
-    def on_load(self):
+    def on_load_textbox(self):
+        self.textbox_text = self.textbox.toPlainText()
+        self.EL.load_text_string(self.textbox_text)
+        self.text_from_file = False
+        self.display_html_content()
+
+    @pyqtSlot()
+    def on_load_file(self):
         self.getFile()
 
-    @pyqtSlot()
     def on_select_ner(self, index):
         self.selectedNER = index
-        print('bloblbloblblRRRRRRRRRRRR', index)
 
-    @pyqtSlot()
     def on_select_ned(self, index):
         self.selectedNED = index
-        print('bloblbloblblDDDDDDDDDDDD', index)
+
+    def set_ner(self):
+        self.is_ner_used = not self.is_ner_used
+
+    def set_ned(self):
+        self.is_ned_used = not self.is_ned_used
 
     @pyqtSlot()
     def on_start(self):
         self.EL.select_ner(self.selectedNER)
         self.EL.select_ned(self.selectedNED)
-        self.EL.load_text(self.path)
+
+        if self.text_from_file:
+            self.EL.load_text(self.path)
+        else:
+            self.EL.load_text_string(self.textbox_text)
         
-        fileName = ntpath.basename(self.path)
-        self.EL.ner()
-        self.EL.ned()
+        if self.is_ner_used:
+            self.EL.ner()
+        if self.is_ned_used:
+            self.EL.ned()
+        
         self.display_html_content()
 
     @pyqtSlot()
-    def on_save(self):
-        print('PyQt5 button click')
+    def on_load_dataset(self):
+        print('Load Dataset')
+
+    @pyqtSlot()
+    def on_start_ned_test(self):
+        print('Start Ned test')
 
     
     def getFile(self):
@@ -239,6 +281,8 @@ class DesktopApplication(QMainWindow):
 
         '''Displaying unedited text'''
         self.EL.load_text(self.path)
+        self.text_from_file = True
+        self.textbox_text = None
         self.display_html_content()
     
 
@@ -246,25 +290,27 @@ class DesktopApplication(QMainWindow):
         self.dom.find("div[id=content]").text(self.EL.text.get_html_text())
         self.html_content = "<!DOCTYPE html>"+self.dom.referenceToRootElement.html() # = file.read()
         # self.app_html_section.setHtml(self.html_content)
-        self.app_html_section.update_html_text(self.html_content)
+        self.update_html_text(self.html_content)
 
-
-class HTMLWindow(QMainWindow):
-    def __init__(self, width, height):
-        super().__init__()
-
-        tfile = open("siem.html", mode= "r" , encoding="utf-8")
-        template = tfile.read()
-        tfile.close()
-
-        self.web_engine_view = QWebEngineView(self)
-        self.web_engine_view.setHtml(template)
-        self.setCentralWidget(self.web_engine_view)
-        self.setFixedSize(width, height)
-    
     def update_html_text(self, content):
-        self.web_engine_view.setHtml(content)
+        self.app_html_section.setHtml(content)
 
+
+class CustomWebEnginePage(QWebEnginePage):
+    """ Custom WebEnginePage to customize how we handle link navigation """
+    def __init__(self, destination, parent=None):
+        super().__init__(parent)
+
+        self.destination = destination
+
+
+    def acceptNavigationRequest(self, url,  _type, isMainFrame):
+        if _type == QWebEnginePage.NavigationTypeLinkClicked:
+            self.destination.setUrl(url)
+
+            return False
+        return super().acceptNavigationRequest(url,  _type, isMainFrame)
+    
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
