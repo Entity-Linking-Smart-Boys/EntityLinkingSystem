06b1fca2e53ea433de10b04abb939cbd8182fb38
Krzysztof Nazar <s184698@student.pg.edu.pl>
2023-12-02 18:24:53 +0100

    update tests

--- tests/tests_NED.py
+++ tests/tests_NED.py
@@ -10,34 +10,35 @@ class TestNEDSelect(unittest.TestCase):
 
     def test_select_ned_Lookup(self):
         ned_id = 0
-        self.EL.select_ned(ned_id)
+        self.EL.select_ned(ned_id, use_NER_class=False)
         selected_ned = self.EL.NED.candidateType
         self.assertEqual(selected_ned, "Lookup")
 
     def test_select_ned_Graph(self):
         ned_id = 1
-        self.EL.select_ned(ned_id)
+        self.EL.select_ned(ned_id, use_NER_class=False)
         selected_ned = self.EL.NED.candidateType
         self.assertEqual(selected_ned, "Graph")
 
     def test_select_ned_Math(self):
         ned_id = 2
-        self.EL.select_ned(ned_id)
+        self.EL.select_ned(ned_id, use_NER_class=False)
         selected_ned = self.EL.NED.candidateType
         self.assertEqual(selected_ned, "Math")
 
 
-class TestNEDRun(unittest.TestCase):
+class TestNEDRunUsingNERClass(unittest.TestCase):
 
     def setUp(self):
         self.EL = EntityLinkingSystem()
         self.test_text = "test_text.txt"
+        self.use_NER_class = True
 
     def test_run_ned_Lookup(self):
         ner_id = 0  # Spacy
         ned_id = 0  # Lookup
         self.EL.select_ner(ner_id)
-        self.EL.select_ned(ned_id)
+        self.EL.select_ned(ned_id, self.use_NER_class)
 
         self.EL.load_text(self.test_text)
 
@@ -51,7 +52,7 @@ class TestNEDRun(unittest.TestCase):
         ner_id = 0  # Spacy
         ned_id = 1  # Graph
         self.EL.select_ner(ner_id)
-        self.EL.select_ned(ned_id)
+        self.EL.select_ned(ned_id, self.use_NER_class)
 
         self.EL.load_text(self.test_text)
 
@@ -65,7 +66,57 @@ class TestNEDRun(unittest.TestCase):
         ner_id = 0  # Spacy
         ned_id = 2  # Math
         self.EL.select_ner(ner_id)
-        self.EL.select_ned(ned_id)
+        self.EL.select_ned(ned_id, self.use_NER_class)
+
+        self.EL.load_text(self.test_text)
+
+        self.EL.ner()
+
+        self.EL.ned()
+
+        self.EL.save_html("test_text_saved.html")
+
+
+class TestNEDRunWihtoutUsingNERClass(unittest.TestCase):
+
+    def setUp(self):
+        self.EL = EntityLinkingSystem()
+        self.test_text = "test_text.txt"
+        self.use_NER_class = False
+
+    def test_run_ned_Lookup(self):
+        ner_id = 0  # Spacy
+        ned_id = 0  # Lookup
+        self.EL.select_ner(ner_id)
+        self.EL.select_ned(ned_id, self.use_NER_class)
+
+        self.EL.load_text(self.test_text)
+
+        self.EL.ner()
+
+        self.EL.ned()
+
+        self.EL.save_html("test_text_saved.html")
+
+    def test_run_ned_Graph(self):
+        ner_id = 0  # Spacy
+        ned_id = 1  # Graph
+        self.EL.select_ner(ner_id)
+        self.EL.select_ned(ned_id, self.use_NER_class)
+
+        self.EL.load_text(self.test_text)
+
+        self.EL.ner()
+
+        self.EL.ned()
+
+        self.EL.save_html("test_text_saved.html")
+
+    def test_run_ned_Math(self):
+        ner_id = 0  # Spacy
+        ned_id = 2  # Math
+        self.EL.select_ner(ner_id)
+        self.EL.select_ned(ned_id, self.use_NER_class)
 
         self.EL.load_text(self.test_text)
 
